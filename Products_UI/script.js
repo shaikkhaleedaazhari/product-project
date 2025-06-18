be718772b8bee31856e8d1024488fb556e898e37
shaikkhaleedaazhari <129606192+shaikkhaleedaazhari@users.noreply.github.com>
2025-06-19 03:48:27 +0530

    Update script.js

--- Products_UI/script.js
+++ Products_UI/script.js
@@ -1,5 +1,5 @@
-// API URL - This should point to your API Gateway
-const API_URL = 'https://localhost:7000/api/products';
+// API URL - This should point to your EC2 backend IP and port
+const API_URL = 'http://18.234.196.58:7001/api/products';
 
 // DOM Elements
 const productsContainer = document.getElementById('products-container');
