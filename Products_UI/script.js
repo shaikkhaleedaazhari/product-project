6328432e5a4ab2e4a3d4b8a8c8f3d2bd34c49aad
Ubuntu <ubuntu@ip-172-31-94-30.ec2.internal>
2025-06-20 13:27:41 +0000

    added some extra

--- Products_UI/script.js
+++ Products_UI/script.js
@@ -1,5 +1,5 @@
 // API URL - This should point to your EC2 backend IP and port
-const API_URL = 'http://products-backend:80/api/products';
+const API_URL = 'http://44.211.251.179:7001/api/products';
 
 // DOM Elements
 const productsContainer = document.getElementById('products-container');
