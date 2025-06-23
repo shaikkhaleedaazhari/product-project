d2c26853486c1a7d2e46f72fe4a59619ec6fc23b
shaikkhaleedaazhari <129606192+shaikkhaleedaazhari@users.noreply.github.com>
2025-06-23 22:13:38 +0530

    Update outputs.tf

--- Infrastructure/Terraform/outputs.tf
+++ Infrastructure/Terraform/outputs.tf
@@ -2,6 +2,14 @@ output "vpc_id" {
   value = module.vpc.vpc_id
 }
 
+output "public_subnet_ids" {
+  value = module.vpc.public_subnets
+}
+
+output "private_subnet_ids" {
+  value = module.vpc.private_subnets
+}
+
 output "eks_cluster_name" {
   value = module.eks.cluster_name
 }
